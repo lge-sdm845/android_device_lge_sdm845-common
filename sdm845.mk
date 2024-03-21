b1c3ff77ff8ca57a884bcea1fbcab2d5eb43baf0
Michael Bestas <mkbestas@lineageos.org>
2024-03-21 13:38:44 +0200

    sdm845-common: Switch to android.hardware.usb@1.3-service.dual_role_usb
    Change-Id: Ibd14f7d31dfaf41e040302eda3bc49f351deb618

--- sdm845.mk
+++ sdm845.mk
@@ -471,13 +471,9 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.lge_sdm845
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.3-service.dual_role_usb
 
 # Vendor libstdc++
 PRODUCT_PACKAGES += \
