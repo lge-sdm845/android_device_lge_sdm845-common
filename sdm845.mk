14bfc1643d4d1cd87b93b9612d0825318363d7b2
Tuan Anh <tuan73176@gmail.com>
2024-04-13 09:22:18 +0700

    sdm845-common: Reland FM
    Change-Id: Ia3a6618bbd97a5e7f7b0268c8c7351612919020c

--- sdm845.mk
+++ sdm845.mk
@@ -198,14 +198,10 @@ PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.lge_sdm845
 
 # FM packages
-#PRODUCT_PACKAGES += \
-#	libqcomfm_jni \
-#	android.hardware.broadcastradio@1.0-impl \
-#	FM2 \
-#	qcom.fmradio \
-#	qcom.fmradio.xml
-
-#PRODUCT_BOOT_JARS += qcom.fmradio
+PRODUCT_PACKAGES += \
+    FM2 \
+    libqcomfm_jni \
+    qcom.fmradio
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
