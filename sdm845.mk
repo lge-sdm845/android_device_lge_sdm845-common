c2b4863966b66d57f28f6e3848326d746090a721
Tuan Anh <tuan73176@gmail.com>
2024-04-21 00:12:24 +0700

    sdm845-common: Cleanup radio targets
    Change-Id: I1db87df93b63a57897efde94e30e3a6929fc2455

--- sdm845.mk
+++ sdm845.mk
@@ -423,11 +423,8 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.5 \
     android.hardware.radio@1.5.vendor \
-    android.hardware.radio.config@1.2 \
     android.hardware.radio.config@1.2.vendor \
-    android.hardware.radio.deprecated@1.0 \
     android.hardware.radio.deprecated@1.0.vendor \
     android.system.net.netd@1.1 \
     android.system.net.netd@1.1.vendor \
