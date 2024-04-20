a2c66f6fb786b65b2a90988c4a6a8713b3e1dada
Nolen Johnson <johnsonnolen@gmail.com>
2024-04-20 21:15:37 +0700

    sdm845: Remove deprecated TARGET_USES_QTI_CAMERA_DEVICE
    * Dead as of https://review.lineageos.org/c/LineageOS/android_vendor_lineage/+/331507.
    * We already build vendor.qti.hardware.camera.device@1.0 so we have what we need.
    
    [Tuan Anh: Rename vendor.qti.hardware.camera.device@1.0]
    
    Change-Id: I27471ae2a29a9538a8222dada5e2665d98edc0f8

--- sdm845.mk
+++ sdm845.mk
@@ -151,7 +151,7 @@ TARGET_SCREEN_WIDTH := 1440
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    vendor.qti.hardware.camera.device@1.0.vendor
+    vendor.qti.hardware.camera.device@1.0
 
 # Context Hub
 PRODUCT_PACKAGES += \
