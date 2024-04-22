8255b7c1ac5581bc541c460a6c75ee4f919a9ee5
David Ng <dave@codeaurora.org>
2024-04-22 08:42:35 +0700

    sdm845-common: config.fs: Add sys_boot cap to peripheral manager
    Enable sys_boot capability to allow system reboot
    to peripheral manager.
    
    Change-Id: I04b67543c4d2e541b9699b6faa3337b2638b3257

--- config.fs
+++ config.fs
@@ -56,7 +56,7 @@ caps: NET_BIND_SERVICE WAKE_ALARM BLOCK_SUSPEND
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
-caps: NET_BIND_SERVICE
+caps: NET_BIND_SERVICE SYS_BOOT
 
 [vendor/bin/pd-mapper]
 mode: 0755
