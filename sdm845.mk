81e59bd3672c733e22d631e112f202ef9bbbd8ab
Steven Moreland <smoreland@google.com>
2024-05-04 14:30:44 +0700

    sdm845-common: Move /vendor/ueventd.rc to /vendor/etc/ueventd.rc
    The root location of this file is deprecated for Android T.
    
    Bug: 189268918
    Bug: 178221726
    Test: device reaches boot complete after raising API level to 32
    Change-Id: I97a6d2e5cacd0ac5eeb81daafeb8b1a0ad5c119b

--- sdm845.mk
+++ sdm845.mk
@@ -269,7 +269,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/etc/fstab.hardware:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.$(DEVICE_NAME) \
     $(COMMON_PATH)/rootdir/etc/init.hardware.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.$(DEVICE_NAME).rc \
-    $(COMMON_PATH)/rootdir/etc/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc
+    $(COMMON_PATH)/rootdir/etc/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/etc/ueventd.rc
 
 $(foreach f,$(wildcard $(COMMON_PATH)/rootdir/etc/init/hw/*.rc),\
         $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/$(notdir $f)))
