469fdc97827739ada4b314f7266881ee6d68bfa4
Michael Bestas <mkbestas@lineageos.org>
2024-06-17 10:41:47 +0300

    sdm845-common: Inherit from legacy QTI FCM
    Change-Id: I0bde1c273d4abe689f61e7d4ebcf80965d785489

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -105,6 +105,7 @@ DEVICE_MATRIX_FILE += hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(COMMON_PATH)/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
     vendor/lineage/config/device_framework_matrix.xml
 
 # Init
