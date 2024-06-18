d108bde13eddc93f140ccbabcbd2559bf0856d2b
Michael Bestas <mikeioannina@gmail.com>
2024-06-18 20:54:45 +0700

    sdm845-common: Include Lineage FCM
    Change-Id: I0bdd4d63b60a1e29cdd69e71f7268932a105c16b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -102,6 +102,8 @@ BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET := true
 # HIDL
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE += hardware/qcom-caf/common/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    vendor/lineage/config/device_framework_matrix.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_lge_sdm845
